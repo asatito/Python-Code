173e800bcdf0b833948e4bad9869c07ac24db50c
Ashoke Sati <ashoke.sati@gmail.com>
2022-05-06 23:06:46 -0400

    Add files via upload
    Initial commit

--- Graph Coursera/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ Graph Coursera/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -6,7 +6,31 @@ import sys
 def number_of_components(adj):
     result = 0
     #write your code here
-    return result
+    
+ # home keeping variables
+    visited = [0]*len(adj)
+    component_num = 0
+    
+    # if the graph is connected then the first call itself will mar all nodes as visited
+    # due to recursive calls made by explore, however for uncconected graphs the for loop
+    # will enter the body of loop and increment the component number
+    for node in range(len(adj)):
+        if not visited[node]:
+            visited[node] = 1
+            component_num += 1            
+            explore(adj, adj[node], visited)
+    
+    return component_num
+    
+
+def explore(adj, start, visited):
+    for neighbour in start:
+         if not visited[neighbour]:
+             visited[neighbour] = 1
+             # please see that this is dfs ..for the neighbour we start exploring its adjacency list adj[neighbour] 
+             # as shown in the call below
+             explore(adj, adj[neighbour], visited)         
+    return
 
 if __name__ == '__main__':
     input = sys.stdin.read()
